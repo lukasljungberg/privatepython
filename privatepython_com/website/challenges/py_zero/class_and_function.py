c6c2d5041b72bf4b2622d7c820e28cbae5e6ebe6
pat0 <lukas-ljungberg@outlook.com>
2025-03-16 00:26:12 +0100

    newCodeTest

---
+++ privatepython_com/website/challenges/py_zero/class_and_function.py
@@ -0,0 +1,33 @@
+# Change here
+class User:
+    x = "USERNAME"
+    def present_user(self):
+        return self.x
+    
+def the_function(print_this: bool = False, the_list: list = None):
+    if print_this:
+        print(''.join(the_list))
+
+# Don't change
+class Person(User):
+    def __init__(self):
+        self.x = "NO_USER"
+    
+    def get_username_as_list(self):
+        return list(self.present_user())
+
+user_person = Person()
+the_function(the_list=user_person.get_username_as_list())
+# STATIC
+# Don't change
+class Person(User):
+    def __init__(self):
+        self.x = "NO_USER"
+    
+    def get_username_as_list(self):
+        return list(self.present_user())
+
+user_person = Person()
+the_function(the_list=user_person.get_username_as_list())
+        
+        
