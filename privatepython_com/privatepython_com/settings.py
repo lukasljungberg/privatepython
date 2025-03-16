c435ffa074f414fede1351fc52e275ff1eab3b91
pat0 <lukas-ljungberg@outlook.com>
2025-03-16 09:36:56 +0100

    commit

--- privatepython_com/privatepython_com/settings.py
+++ privatepython_com/privatepython_com/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'privatepython_com.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    # Don't forget to pip install psycopg2-binary on the server
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'yourdbname',
+            'USER': 'yourdbuser',
+            'PASSWORD': 'yourpassword',
+            'HOST': 'yourdbhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
