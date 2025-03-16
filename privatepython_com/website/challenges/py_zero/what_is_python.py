584a00e3c64ae62caf10a5485e2e066f2ebe33ae
pat0 <lukas-ljungberg@outlook.com>
2025-03-16 11:12:21 +0100

    commit

--- privatepython_com/website/challenges/py_zero/what_is_python.py
+++ privatepython_com/website/challenges/py_zero/what_is_python.py
@@ -1,10 +1,13 @@
 # This is a comment
 # Run the code to say "Hello USERNAME!"
+# Don't change
 x = "USERNAME"
-y = None
+# Change
 y = y
 # Don't change
 print(y)
 # STATIC
+# Don't change
+x = "USERNAME"
 # Don't change
 print(y)
